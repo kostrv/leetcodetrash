0186452fb97d8c8b4768aa91f9ce40176f97f694
Nik <gaponov.nik.07@gmail.com>
2026-02-20 00:31:15 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (94.18%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,24 +1,24 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        left_boundary = 0
-        right_boundary = len(letters) - 1
+        left_b = 0
+        right_b = len(letters) - 1
         
         # If target is greater than or equal to the last letter,
         # wrap around and return the first letter as per the problem rules.
-        if target >= letters[right_boundary]:
+        if target >= letters[right_b]:
             return letters[0]
 
-        while left_boundary <= right_boundary:
-            middle_index = (left_boundary + right_boundary) // 2
+        while left_b <= right_b:
+            middle_index = (left_b + right_b) // 2
             current_char = letters[middle_index]
             
             if current_char <= target:
                 # If current char is not greater than target, search in the right half.
-                left_boundary = middle_index + 1
+                left_b = middle_index + 1
             else:
                 # If current char is greater, it could be our answer, 
                 # but we keep searching left to find the smallest possible greater char.
-                right_boundary = middle_index - 1
+                right_b = middle_index - 1
                 
-        # After the loop, left_boundary points to the smallest char strictly greater than target.
-        return letters[left_boundary]
+        # After the loop, left_b points to the smallest char strictly greater than target.
+        return letters[left_b]
