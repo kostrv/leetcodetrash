81dc0cde97aadb677283b1a9472c217c39e907da
Nik <gaponov.nik.07@gmail.com>
2026-02-24 23:07:35 +0500

    Time: 88 ms (53.60%) | Memory: 31 MB (57.92%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -7,7 +7,7 @@ class Solution:
         for num in nums:
             heapq.heappush(heap, num)
             
-            if len(heap) > k:
+            if len(heap) > k: # Используем данное условие для того, чтобы получить k в корне кучи
                 heapq.heappop(heap)
         
         return heap[0]
