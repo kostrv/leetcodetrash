9bb33da1cd2bd937514a704a04133989f1da6658
Nik <gaponov.nik.07@gmail.com>
2026-03-19 00:00:29 +0500

    Time: 7 ms (48.00%) | Memory: 22.3 MB (91.63%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,26 +1,29 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.minStack = []
+        self.stack = [] # основной стек для хранения всех значений
+        self.minStack = [] # стек для хранения минимальных значений
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
+        self.stack.append(val) # добавляем элемент в основной стек
         
+        # если новый элемент меньше или равен текущему минимуму, добавляем его в стек минимумов
         if len(self.minStack) == 0 or val <= self.minStack[-1]:
-            self.minStack.append(val)
+            self.minStack.append(val) 
     
-    def pop(self) -> None:
+    def pop(self) -> None: 
+        # удаляем верхний элемент из основного стека
         top = self.stack[-1]
         self.stack.pop()
         
+        # если удаленный элемент был текущим минимумом, удаляем его из стека минимумов
         if top == self.minStack[-1]:
             self.minStack.pop()
 
-    def top(self) -> int:
+    def top(self) -> int: # возвращаем верхний элемент основного стека
         return self.stack[-1]
 
-    def getMin(self) -> int:
+    def getMin(self) -> int: # возвращаем верхний элемент стека минимумов, который является текущим минимумом
         return self.minStack[-1]
 
 
