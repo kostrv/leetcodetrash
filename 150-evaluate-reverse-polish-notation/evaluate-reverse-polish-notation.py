57364fec9a7d919eb652182d87ee9c080480d0cb
Nik <gaponov.nik.07@gmail.com>
2026-03-19 01:02:12 +0500

    Time: 3 ms (57.16%) | Memory: 20.7 MB (30.82%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,22 +1,27 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         self.stack = []
+        
+        # операции для выполнения в зависимости от оператора
         self.operations = {
-            "+" : lambda a, b: a + b,
-            "-" : lambda a, b: a - b,
-            "*" : lambda a, b: a * b,
-            "/" : lambda a, b: int(a / b),
+            '+' : lambda a, b: a + b,
+            '-' : lambda a, b: a - b,
+            '*' : lambda a, b: a * b,
+            '/' : lambda a, b: int(a / b),
         }
 
         for el in tokens:
-            if el in ["+", "-", "*", "/"]:
+            # если элемент - оператор, выполняем соответствующую операцию над последними двумя элементами в стекe
+            if el in ['+', '-', '*', '/']: 
                 if len(self.stack) != 0:
                     first = self.stack.pop()
                     second = self.stack.pop()
 
+                    # выполняем операцию и добавляем результат обратно в стек
                     new_val = self.operations[el](int(second), int(first))
                     self.stack.append(new_val)
             else:
                self.stack.append(el)
-    
+
+        # в конце в стеке останется единственный элемент - результат вычисления выражения, который мы возвращаем
         return int(self.stack[-1])
