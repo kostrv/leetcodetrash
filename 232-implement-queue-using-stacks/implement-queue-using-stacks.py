5086b767aead1cc3296731f94b15a22d09a8b6d3
Nik <gaponov.nik.07@gmail.com>
2026-03-19 20:19:41 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (76.14%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,26 +1,30 @@
 class MyQueue:
-
     def __init__(self):
+        # Используем два стека: один для хранения входящих элементов, другой для хранения элементов в порядке очереди
         self.stack = []
-        self.queue = []
+        self.queue = [] 
 
     def push(self, x: int) -> None:
         self.stack.append(x)
 
     def refill(self) -> None:
+        # Функция для переноса элементов из стека в очередь, если очередь пуста. 
         if len(self.queue) == 0:
             while len(self.stack) != 0:
                 self.queue.append(self.stack.pop())
 
     def pop(self) -> int:
+        # Перед извлечением элемента из очереди, проверяем, нужно ли нам заполнить её элементами из стека.
         self.refill()
         return self.queue.pop()
         
     def peek(self) -> int:
+        # Перед просмотром первого элемента очереди, проверяем, нужно ли нам заполнить её элементами из стека.
         self.refill()
         return self.queue[-1]
 
     def empty(self) -> bool:
+        # Очередь считается пустой, если оба стека - и входящий, и очередь - пусты.
         return len(self.queue) == 0 and len(self.stack) == 0 
 
 
