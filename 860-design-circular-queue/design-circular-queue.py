377bfd48ed0f96f530385258740950da17dcbe89
Nik <gaponov.nik.07@gmail.com>
2026-03-19 23:58:03 +0500

    Time: 3 ms (86.67%) | Memory: 19.9 MB (79.78%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,16 +1,19 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.head = 0
-        self.tail = 0
-        self.size = 0
-        self.capacity = k        
-        self.queue = [0] * self.capacity
+        self.head = 0 # указатель на начало очереди
+        self.tail = 0 # указатель на конец очереди
+        self.size = 0 # текущий размер очереди
+        self.capacity = k # максимальная емкость очереди
+                
+        # инициализируем очередь фиксированного размера
+        self.queue = [0] * self.capacity 
         
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-        
+
+        # добавляем элемент в позицию, на которую указывает tail, и затем перемещаем tail вперед по кругу        
         self.queue[self.tail] = value
         self.tail = (self.tail + 1) % self.capacity
         self.size += 1
@@ -21,6 +24,7 @@ class MyCircularQueue:
         if self.isEmpty():
             return False
 
+        # удаляем элемент в позиции, на которую указывает head, и затем перемещаем head вперед по кругу
         self.queue[self.head] = 0
         self.head = (self.head + 1) % self.capacity
         self.size -= 1
@@ -30,19 +34,23 @@ class MyCircularQueue:
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-
+        
+        # возвращаем элемент в позиции, на которую указывает head, который является передним элементом очереди
         return self.queue[self.head]
 
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
 
+        # возвращаем элемент в позиции, на которую указывает tail - 1, который является задним элементом очереди
         return self.queue[(self.tail - 1) % self.capacity]
 
     def isEmpty(self) -> bool:
+        # очередь считается пустой, если размер равен нулю.
         return self.size == 0
 
     def isFull(self) -> bool:
+        # очередь считается полной, если размер равен максимальной емкости.
         return self.size == self.capacity
 
 
