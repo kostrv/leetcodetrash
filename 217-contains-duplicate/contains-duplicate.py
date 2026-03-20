e874b2dcc568fa12cd07097d382c38b2eb64bb03
Nik <gaponov.nik.07@gmail.com>
2026-03-20 10:13:37 +0500

    Time: 25 ms (16.32%) | Memory: 37.4 MB (5.86%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        self.vals = {}
+
+        for i in nums:
+            if i in self.vals.keys():
+                self.vals[i] += 1 
+                return True
+            else:
+                self.vals[i] = 1
+        return False
