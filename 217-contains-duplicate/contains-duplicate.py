f2daafccd0ce9a1992b81a72f7981eae98bfe226
Nik <gaponov.nik.07@gmail.com>
2026-03-20 10:15:33 +0500

    Time: 23 ms (18.38%) | Memory: 36.1 MB (17.22%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -8,4 +8,15 @@ class Solution:
                 return True
             else:
                 self.vals[i] = 1
+        
         return False
+
+        # self.vals = []
+
+        # for i in nums:
+        #     if i in self.vals:
+        #         return True
+        #     else:
+        #         self.vals.append(i)
+        
+        # return False
