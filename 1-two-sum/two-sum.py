049eb46fa9d8cdc2f94ea48049541710bd351a58
Nik <gaponov.nik.07@gmail.com>
2026-03-20 10:21:33 +0500

    Time: 2 ms (55.73%) | Memory: 20.5 MB (36.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,5 +5,6 @@ class Solution:
         for id, num in enumerate(nums):
             diff = target - num
 
-            if diff in self.values.keys(): return [self.values[diff], id]
+            if diff in self.values: 
+                return [self.values[diff], id]
             self.values[num] = id
