d649173a1d104e849cd3084d21505bd7cc826b04
Nik <gaponov.nik.07@gmail.com>
2026-03-20 10:37:57 +0500

    Time: 15 ms (43.04%) | Memory: 19.5 MB (40.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -17,11 +17,11 @@ class Solution:
         #     else:
         #         self.second[i] = 1
         
-        # for i in range(len(s)):
-        #     self.first[s[i]] = 1 + self.first.get(s[i], 0) 
-        #     self.second[t[i]] = 1 + self.second.get(t[i], 0)
+        for i in range(len(s)):
+            self.first[s[i]] = 1 + self.first.get(s[i], 0) 
+            self.second[t[i]] = 1 + self.second.get(t[i], 0)
 
-        # return self.first == self.second
+        return self.first == self.second
 
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
 
