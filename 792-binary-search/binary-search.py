b04725c3b4d0c8fbdc1c0ae9e119b047d2e2a533
Nik <gaponov.nik.07@gmail.com>
2026-03-29 00:47:11 +0500

    Time: 0 ms (100.00%) | Memory: 20.5 MB (58.89%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,20 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Define the left and right boundaries of the search space
         left_b = 0
         right_b = len(nums) - 1
+
         
-        while left_b <= right_b: # If we gets here, it means we have exhausted the search space and haven't found the target
+        while left_b <= right_b:
             middle_index = (left_b + right_b) // 2
-            current_element = nums[middle_index]
+            current_el = nums[middle_index]
 
-            if current_element == target: # Successfully found the target, return the index
+            if current_el == target:
                 return middle_index
-
-            # Move the boundaries
-            if current_element < target: 
+            
+            if current_el < target:
                 left_b = middle_index + 1
-            else: 
+            else:
                 right_b = middle_index - 1
 
         return -1
+        
