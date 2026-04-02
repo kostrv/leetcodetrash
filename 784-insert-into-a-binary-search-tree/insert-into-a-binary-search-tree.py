792fc133418fa4bedc7983eaed34ff7a55dcbe30
Nik <gaponov.nik.07@gmail.com>
2026-04-03 00:38:52 +0500

    Time: 8 ms (7.25%) | Memory: 21.2 MB (67.12%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -2,28 +2,29 @@
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         self.last = None
-
+        
         def insert(node, pos=None):
             if node is None:
-                new_node = TreeNode(val)
+                new_node = TreeNode(val, None, None)
                 if self.last is None:
+                    # если это самый первый узел, возвращаем его
                     return new_node
-                
+                    
                 if pos == 'right':
                     self.last.right = new_node
                 elif pos == 'left':
                     self.last.left = new_node
-                return new_node # Возвращаем созданный узел
+                return new_node
 
             self.last = node
             if node.val < val:
-                # ТУТ НУЖЕН RETURN, чтобы прокинуть созданный узел наверх
-                return insert(node.right, pos='right') 
-            else:
-                # И ТУТ ТОЖЕ
+                return insert(node.right, pos='right')
+            elif node.val > val:
                 return insert(node.left, pos='left')
+            
+            return node 
 
-        # Если дерево пустое, insert вернет новый узел
-        # Если не пустое, нам нужно вернуть ИСХОДНЫЙ root
+        # если дерево пустое, insert вернет новый узел
+        # если не пустое, нам нужно вернуть ИСХОДНЫЙ root
         res = insert(root)
         return root if root else res
