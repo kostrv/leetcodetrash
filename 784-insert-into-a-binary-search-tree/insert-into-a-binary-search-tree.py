bac0561c837b54e10e875dc2b4438ee69ac7885d
Nik <gaponov.nik.07@gmail.com>
2026-04-03 00:41:25 +0500

    Time: 4 ms (19.63%) | Memory: 21.1 MB (89.96%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -1,15 +1,47 @@
 
+# class Solution:
+#     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         self.last = None
+        
+#         def insert(node, pos=None):
+#             if node is None:
+#                 new_node = TreeNode(val, None, None)
+#                 if self.last is None:
+#                     # если это самый первый узел, возвращаем его
+#                     return new_node
+                    
+#                 if pos == 'right':
+#                     self.last.right = new_node
+#                 elif pos == 'left':
+#                     self.last.left = new_node
+#                 return new_node
+
+#             self.last = node
+#             if node.val < val:
+#                 return insert(node.right, pos='right')
+#             elif node.val > val:
+#                 return insert(node.left, pos='left')
+            
+#             return node 
+
+#         # если дерево пустое, insert вернет новый узел
+#         # если не пустое, нам нужно вернуть ИСХОДНЫЙ root
+#         res = insert(root)
+#         return root if root else res
+
+
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         self.last = None
-        
+
         def insert(node, pos=None):
             if node is None:
-                new_node = TreeNode(val, None, None)
+                new_node = TreeNode(val)
                 if self.last is None:
-                    # если это самый первый узел, возвращаем его
+                    # Если это самый первый узел, возвращаем его
                     return new_node
-                    
+                
+                # Привязываем к родителю
                 if pos == 'right':
                     self.last.right = new_node
                 elif pos == 'left':
@@ -18,13 +50,12 @@ class Solution:
 
             self.last = node
             if node.val < val:
-                return insert(node.right, pos='right')
+                insert(node.right, pos='right')
             elif node.val > val:
-                return insert(node.left, pos='left')
+                insert(node.left, pos='left')
             
-            return node 
+            return node # Возвращаем текущий узел дальше по цепочке
 
-        # если дерево пустое, insert вернет новый узел
-        # если не пустое, нам нужно вернуть ИСХОДНЫЙ root
-        res = insert(root)
-        return root if root else res
+        # Перезаписываем root результатом функции
+        root = insert(root)
+        return root
