af174f9004330484453c143e23349670002f8b79
Nik <gaponov.nik.07@gmail.com>
2026-04-17 10:36:10 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (32.56%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        count = len(nums)
+
+        if count == 0: return 0
+        if count == 1: return nums[0]
+
+        amounts = [0] * count
+        
+        amounts[0] = nums[0]
+        amounts[1] = max(nums[1], nums[0])
+
+        for i in range(2, count):
+            previous = amounts[i-1]
+            current_and_previous = nums[i] + amounts[i-2]
+            amounts[i] = max(previous, current_and_previous)
+
+        return amounts[count-1]
+        
