18738259ec2e72410535e8b282c40e07a98a8fd7
Nik <gaponov.nik.07@gmail.com>
2026-04-17 10:02:21 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.16%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        self.prev = [0] * (n + 1)
+        
+        
+        self.prev[1] = 1
+        self.prev[2] = 2
+
+        for i in range(3, n + 1):
+            self.prev[i] = self.prev[i-1] + self.prev[i-2]
+
+        return self.prev[i]
+
+
