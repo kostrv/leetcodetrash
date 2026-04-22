6ccc5e2816b8a27a7b695a5e6257c35fc0b05db3
Nik <gaponov.nik.07@gmail.com>
2026-04-23 00:29:05 +0500

    Time: 317 ms (98.77%) | Memory: 20.1 MB (33.90%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -6,7 +6,7 @@ class Solution:
         visited = {0}
         num_coins = 0
         
-        # Строим уровни нашего "треугольника", пока они не закончатся
+        
         while current_layer:
             num_coins += 1
             next_layer = set()
@@ -16,13 +16,13 @@ class Solution:
                     new_total = total + coin
                     
                     if new_total == amount:
-                        return num_coins # Мы достигли вершины amount!
+                        return num_coins 
                     
                     if new_total < amount and new_total not in visited:
                         visited.add(new_total)
                         next_layer.add(new_total)
             
-            # Переходим к следующему ряду треугольника
+         
             current_layer = next_layer
             
-        return -1 # Если треугольник "закрылся", а вершину amount не нашли
+        return -1 
