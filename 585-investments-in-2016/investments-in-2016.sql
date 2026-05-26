9f66c5304d933d0259e6b178a50db1c8f148d178
Nik <gaponov.nik.07@gmail.com>
2026-05-27 01:13:35 +0500

    Time: 610 ms (33.24%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,28 +1,29 @@
 
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM Insurance
-WHERE tiv_2015 IN (
-    -- Условие 1: tiv_2015 должен повторяться
-    SELECT tiv_2015 
-    FROM Insurance 
-    GROUP BY tiv_2015 
-    HAVING COUNT(*) > 1
-)
-AND (lat, lon) IN (
-    -- Условие 2: локация должна быть уникальной (встречается только 1 раз)
-    SELECT lat, lon 
-    FROM Insurance 
-    GROUP BY lat, lon 
-    HAVING COUNT(*) = 1
-);
-
-
 -- SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
--- FROM (
---     SELECT 
---         tiv_2016,
---         COUNT(*) OVER(PARTITION BY tiv_2015) AS count_tiv_2015,
---         COUNT(*) OVER(PARTITION BY lat, lon) AS count_location
---     FROM Insurance
--- ) AS sub
--- WHERE count_tiv_2015 > 1 AND count_location = 1;
+-- FROM Insurance
+-- WHERE tiv_2015 IN (
+--     -- Условие 1: tiv_2015 должен повторяться
+--     SELECT tiv_2015 
+--     FROM Insurance 
+--     GROUP BY tiv_2015 
+--     HAVING COUNT(*) > 1
+-- )
+-- AND (lat, lon) IN (
+--     -- Условие 2: локация должна быть уникальной (встречается только 1 раз)
+--     SELECT lat, lon 
+--     FROM Insurance 
+--     GROUP BY lat, lon 
+--     HAVING COUNT(*) = 1
+-- );
+
+
+SELECT ROUND(SUM(t.tiv_2016), 2) AS tiv_2016
+FROM 
+(
+    SELECT 
+        tiv_2016, 
+        COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,
+        COUNT(*) OVER (PARTITION BY lat, lon) AS loc_count
+    FROM Insurance
+)t
+WHERE tiv_2015_count > 1 AND loc_count = 1
